7359799e9e9658a947afadd5e7c214dce111dbd9
IPessers <ivan.pessers.ipl@gmail.com>
2018-08-29 00:04:56 +0200

    Added equalizer freq

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const synth = new Tone.Synth({
     envelope: {
         attack: 1
     }
-}).toMaster()
-synth.triggerAttackRelease('C4', 1);
+}).toMaster();
+
+const EQUALIZER_CENTER_FREQUENCIES = [
+    315,400,500,
+    630,800,1000,
+    1250,1600,2000,
+    2500,3150,4000,
+    5000,6300,8000,
+    10000,12500,16000,
+    20000,
+];
 
 fetch('./assets/Bruit_rose_10s.wav')
     .then(response => response.arrayBuffer()) // receive
